13e6c90e18be80dcf33488ee2b2430397ee9648e
lmk0420 <lmk19960420@gmail.com>
2020-04-15 00:05:16 +0900

    add componet for navigation-bar and delete unnecessary files

---
+++ front/src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <div id="nav1">
+      <NavigationBar></NavigationBar>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+import NavigationBar from '@/components/NavigationBar.vue';
+
+@Component({
+  components: {
+    NavigationBar,
+  }
+})
+export default class Home extends Vue {}
+</script>
---
+++ front/src/components/NavigationBar.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <v-app-bar
+      color="deep-purple accent-4"
+      dense
+      dark
+    >
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-toolbar-title>Calendar</v-toolbar-title>
+
+    </v-app-bar>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+
+@Component
+export default class NavigationBar extends Vue {}
+</script>
