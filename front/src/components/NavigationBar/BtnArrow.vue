72e5af07480ab12c0d91043592b8068bd852f65b
lmk0420 <lmk19960420@gmail.com>
2020-04-19 21:08:13 +0900

    add btn-allow

---
+++ front/src/components/NavigationBar/BtnArrow.vue
@@ -0,0 +1,16 @@
+<template>
+    <v-btn tile large color="teal" icon> 
+      <v-icon v-if="isLeft">keyboard_arrow_left</v-icon>
+      <v-icon v-if="!isLeft">keyboard_arrow_right</v-icon>
+    </v-btn>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator'
+
+@Component
+export default class BtnArrowLeft extends Vue {
+  @Prop()
+  public isLeft?: boolean;
+}
+</script>
--- front/src/components/NavigationBar/NavigationBar.vue
+++ front/src/components/NavigationBar/NavigationBar.vue
@@ -4,14 +4,22 @@
       dense
     >
       <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-icon>calendar_today</v-icon>
       <v-toolbar-title>Calendar</v-toolbar-title>
+      <BtnAllow :isLeft=true></BtnAllow>
+      <BtnAllow :isLeft=false></BtnAllow>
     </v-app-bar>
   </div>
 </template>
 
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator'
+import BtnAllow from '@/components/NavigationBar/BtnArrow.vue';
 
-@Component
+@Component({
+  components: {
+    BtnAllow
+  }
+})
 export default class NavigationBar extends Vue {}
 </script>
