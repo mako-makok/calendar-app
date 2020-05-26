fb77c675af81150f6b806f2f0638a2f439cce768
lmk0420 <lmk19960420@gmail.com>
2020-05-26 20:51:37 +0900

    カレンダーに表示する日付を取得するメソッドを追加

--- front/src/components/Calendar/CalendarBoard.vue
+++ front/src/components/Calendar/CalendarBoard.vue
@@ -15,11 +15,66 @@
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator'
 import CardForDay from '@/components/Calendar/CardForDay.vue'
+import { ScheduleModule } from '@/store/modules/schedule'
 
 @Component({
   components: {
     CardForDay
   }
 })
-export default class CalendarBoard extends Vue {}
+export default class CalendarBoard extends Vue {
+  get dispDates(): number[] {
+    const currentDate = ScheduleModule.currentDate
+
+    const beginOfMonth = new Date(
+      currentDate.getFullYear(),
+      currentDate.getMonth(),
+      1
+    )
+    const day = beginOfMonth.getDay()
+    const dates: number[] = []
+
+    // 月初が月曜日の場合は、先月分の日付を取得しない
+    if (day > 1) {
+      const endOfLastMonthForDay = new Date(
+        currentDate.getFullYear(),
+        currentDate.getMonth(),
+        0
+      ).getDay()
+      for (let i = endOfLastMonthForDay - 1; i >= 0; i--) {
+        dates.push(endOfLastMonthForDay - i)
+      }
+    }
+
+    // 月初が日曜の場合は先月の月曜から土曜を加える
+    if (day === 0) {
+      const endOfLastMonthForDate = new Date(
+        currentDate.getFullYear(),
+        currentDate.getMonth(),
+        0
+      ).getDate()
+      for (let i = endOfLastMonthForDate - 5; i <= endOfLastMonthForDate; i++) {
+        dates.push(i)
+      }
+    }
+
+    const endOfMonth = new Date(
+      currentDate.getFullYear(),
+      currentDate.getMonth() + 1,
+      0
+    )
+    for (let i = beginOfMonth.getDate(); i <= endOfMonth.getDate(); i++) {
+      dates.push(i)
+    }
+
+    // 月末が日曜終わりではない場合は来月の日曜日までの日付を取得する
+    if (endOfMonth.getDay() !== 0) {
+      for (let i = 1; i <= 7 - endOfMonth.getDay(); i++) {
+        dates.push(i)
+      }
+    }
+
+    return dates
+  }
+}
 </script>
