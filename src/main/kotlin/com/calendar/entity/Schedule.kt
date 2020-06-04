bf0dfd4a38ca60715608e2c835df15c4f905530e
lmk0420 <lmk19960420@gmail.com>
2020-06-04 22:29:26 +0900

    LocalDateを使用する

--- src/main/kotlin/com/calendar/entity/Schedule.kt
+++ src/main/kotlin/com/calendar/entity/Schedule.kt
@@ -2,7 +2,7 @@ package com.calendar.entity
 
 import org.seasar.doma.*
 import org.seasar.doma.jdbc.entity.NamingType
-import java.util.Date
+import java.time.LocalDate
 
 @Entity(immutable = true, naming = NamingType.LENIENT_SNAKE_LOWER_CASE)
 @Table(name = "schedule")
@@ -13,6 +13,6 @@ data class Schedule(
     val userId: Int,
     val title: String,
     val description: String,
-    val dateStart: Date,
-    val dateEnd: Date
+    val dateStart: LocalDate,
+    val dateEnd: LocalDate
 )
