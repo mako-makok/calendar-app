a58b35cfac8a2f3ba9e775d15ece0976f0ea1118
lmk0420 <lmk19960420@gmail.com>
2020-06-06 17:07:50 +0900

    Vue Routerを削除

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,5 +1,17 @@
 <template>
   <div id="app">
-    <router-view />
+    <Home />
   </div>
 </template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+import Home from '@/components/Home.vue'
+
+@Component({
+  components: {
+    Home
+  }
+})
+export default class App extends Vue {}
+</script>
