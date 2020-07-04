0ff200ee9a378915563d2609f8ddbce4ca2803db
lmk0420 <lmk19960420@gmail.com>
2020-07-04 20:57:12 +0900

    [fix]datepickerが動かなかったので修正

--- front/src/components/Calendar/AddSchedule/FormDatePicher.vue
+++ front/src/components/Calendar/AddSchedule/FormDatePicher.vue
@@ -8,13 +8,7 @@
     min-width="290px"
   >
     <template v-slot:activator="{ on }">
-      <v-text-field
-        class="date-text-field"
-        v-model="formatedDate"
-        @blur="date = parseDate(dateFormated)"
-        readonly
-        v-on="on"
-      />
+      <v-text-field class="date-text-field" v-model="date" readonly v-on="on" />
     </template>
     <v-date-picker
       v-model="date"
@@ -34,28 +28,8 @@ import { Vue, Component, Prop } from 'vue-property-decorator'
 export default class FormDatePiicker extends Vue {
   @Prop({ required: true })
   private propDate!: number[]
-  private date = this.propDate
+  private date = this.propDate.join('-')
   private menu = false
-
-  private formatDate(date: string) {
-    if (!date) return null
-
-    const [year, month, day] = date.split('-')
-    return `${year}年${month}月${day}日`
-  }
-
-  private parseDate(date: string) {
-    if (!date) return null
-
-    const year = date.substring(0, 4)
-    const month = date.substring(5, 7)
-    const day = date.substring(8, 10)
-    return `${year}-${month}-${day}`
-  }
-
-  private get formatedDate() {
-    return this.formatDate(this.propDate.join('-'))
-  }
 }
 </script>
 
