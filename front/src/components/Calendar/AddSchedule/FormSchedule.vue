455a4dd49ca3f66978002590e0fed5e27c85eb95
lmk0420 <lmk19960420@gmail.com>
2020-07-04 20:58:53 +0900

    [add]時間をバインドするようにする

--- front/src/components/Calendar/AddSchedule/FormSchedule.vue
+++ front/src/components/Calendar/AddSchedule/FormSchedule.vue
@@ -18,7 +18,7 @@
             <FormDatePicher :propDate="date" />
           </v-col>
           <v-col v-if="useTime" :cols="2">
-            <v-combobox v-model="startTime" :items="times" />
+            <v-combobox v-model="timeStart" :items="times" />
           </v-col>
           <v-col :cols="1" align-self="center" style="text-align: center;">
             <div class="hyphen-to">
@@ -26,13 +26,13 @@
             </div>
           </v-col>
           <v-col v-if="useTime" :cols="2">
-            <v-combobox v-model="endTime" :items="times" />
+            <v-combobox v-model="timeEnd" :items="times" />
           </v-col>
           <v-col>
             <FormDatePicher :propDate="date" />
           </v-col>
           <v-col v-if="!useTime" :cols="3" align-self="center">
-            <v-btn small text outlined width="80%" @click="useTime = true">
+            <v-btn small text outlined width="80%" @click="setupUseTime">
               時間を追加
             </v-btn>
           </v-col>
@@ -75,9 +75,18 @@ export default class FormSchedule extends Vue {
   @Prop() propSchedule!: Schedule
   private schedule = this.propSchedule
   private useTime = false
+  private timeStart!: string
+  private timeEnd!: string
   private times = [...Array(96).keys()].map(
     i => `${('00' + Math.floor((i + 1) / 4)).slice(-2)}:${('00' + (((i + 1) * 15) % 60)).slice(-2)}`
   )
+
+  private setupUseTime() {
+    const now = new Date()
+    this.timeStart = `${now.getHours()}:${now.getMinutes()}`
+    this.timeEnd = `${now.getHours()}:${now.getMinutes()}`
+    this.useTime = true
+  }
 
   @Emit()
   private clickedRegister() {
